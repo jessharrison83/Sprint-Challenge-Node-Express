a7e8d643d700652ba039956344d676d21521de3d
Jess <jessdev83@gmail.com>
2018-12-08 13:08:50 -0500

    Projects Router
    projectRouter.js CRUD operations completed and working. using endpoint /actions/id gives list of actions for specific project ID.

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -2,4 +2,82 @@ const express = require("express");
 const router = express.Router();
 const projectDB = require("../data/helpers/projectModel");
 
+router.get("/", (req, res) => {
+  projectDB
+    .get()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(400).json({ message: "projects not found" });
+    });
+});
+
+router.get("/actions/:id", (req, res) => {
+  const { id } = req.params;
+  projectDB
+    .getProjectActions(id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(err => {
+      res.status(404).json({ message: "No projects with that ID." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  projectDB
+    .get(id)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res
+        .status(404)
+        .json({ message: "A project with that ID does not exist." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  projectDB
+    .insert(project)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Post not created" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const project = req.body;
+  const { id } = req.params;
+  projectDB
+    .update(id, project)
+    .then(project => {
+      if (project !== null) {
+        res.json(project);
+      } else {
+        res.status(404).json({ message: "ID not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "project not updated" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  projectDB
+    .remove(id)
+    .then(count => {
+      res.json({ message: `successfully deleted ${count} record(s)` });
+    })
+    .catch(err => {
+      res.status(404).json({ message: "id not found" });
+    });
+});
+
 module.exports = router;

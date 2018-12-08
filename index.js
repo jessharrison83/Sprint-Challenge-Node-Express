b59f7b6417397260f03114bf9dd3a44b9f24e9ff
Jess <jessdev83@gmail.com>
2018-12-08 12:38:45 -0500

    File structure, index.js
    routers folder and files added, index.js boilerplate complete.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const server = express();
 const PORT = 5050;
+const projectRouter = require("./routers/projectRouter");
+const actionRouter = require("./routers/actionRouter");
 //Middleware
 const morgan = require("morgan");
 const helmet = require("helmet");
 
 server.use(express.json(), morgan("tiny"), helmet());
+
+server.use("/projects", projectRouter);
+server.use("/actions", actionRouter);
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
---
+++ routers/projectRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const projectDB = require("../data/helpers/projectModel");
+
+module.exports = router;
